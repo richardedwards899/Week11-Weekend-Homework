adc14317223b08376e92fd047d70c68442ca12a5
Richard Edwards <richardedwards899@gmail.com>
2017-06-25 22:25:05 +0100

    about to overwrite the description

--- public/scripts.js
+++ public/scripts.js
@@ -6,8 +6,8 @@ I chose to work with the Pokemon API because it has lots of nested URLs,
 which will give me practice at working with multiple Http requests.
 
 The makeRequest function is pretty cool. It reads a url, converts the json
-found at that url into js objects, then passes them into a named function. It
-might be *cool*, but it's a bugger to use because I can't return anything
+found at that url into js objects, then passes them into a named function. While
+might be *cool*, it's a bugger to use because I can't return anything
 from it! Maybe I should be using binds?
 */
 
@@ -54,33 +54,59 @@ var createPokeFigure = function(pokemon){
   var caption = document.createElement('figcaption');
   caption.innerText = pokemon.name;
 
-  var img = document.createElement('img');
-  img.classList.add('poke-picture');
-  img.id = pokemon.name;
-  img.src = "images/loading.jpg";
+  var imageDiv = document.createElement('div');
+  imageDiv.classList.add('poke-panel');
 
-  makeRequest(pokemon.url, getImageURLs);
+  var imgFront = document.createElement('img');
+  imgFront.classList.add('poke-picture');
+  imgFront.id = pokemon.name+"-front";
+  imgFront.src = "images/loading.jpg";
+
+  var imgBack = document.createElement('img');
+  imgBack.classList.add('poke-picture');
+  imgBack.id = pokemon.name+"-back";
+  imgBack.src = "images/loading.jpg";
+
+  var description = document.createElement('p');
+  description.id = pokemon.name+"-description";
+  description.innerText = "Description loading - please wait! It'll be worth it.";
+
+  makeRequest(pokemon.url, overwriteDefaultImages);
+
+  console.log("imgFront: ", imgFront);
+  imageDiv.appendChild(imgFront);
+
+  console.log("imgBack: ", imgBack);
+  imageDiv.appendChild(imgBack);
 
   pokeFigure.appendChild(caption);
-
-  console.log("img: ", img);
-  pokeFigure.appendChild(img);
+  pokeFigure.appendChild(imageDiv);
+  pokeFigure.appendChild(description);
 
   return pokeFigure;
 }
 
-var getImageURLs = function(pokemonObjects){
+var overwriteDefaultImages = function(pokemonObjects){
   console.log("pokemonObjects: ", pokemonObjects);
 
   //now we've got the objects, we can get the required URL!
-  var picURL = pokemonObjects.sprites.front_shiny;
-  console.log("picURL for "+pokemonObjects.name, picURL);
+  var picURLFront = pokemonObjects.sprites.front_shiny;
+  console.log("picURLFront for "+pokemonObjects.name, picURLFront);
+
+  var picURLBack = pokemonObjects.sprites.back_shiny;
+  console.log("picURLBack for "+pokemonObjects.name, picURLBack);
 
   //we now have the URL. Just need to find the right object in the DOM
-  //and write to it
-  var imgToOverwrite = document.getElementById(pokemonObjects.name);
-  imgToOverwrite.src = picURL;
+  //and write to it.
+  var imgToOverwriteFront = document.getElementById(pokemonObjects.name+"-front");
+  imgToOverwriteFront.src = picURLFront;
 
+  var imgToOverwriteBack = document.getElementById(pokemonObjects.name+"-back");
+  imgToOverwriteBack.src = picURLBack;
+
+  makeRequest(pokemonObjects.species.url, overwriteDescription);
 }
+
+
 
 window.addEventListener('load', loadObjects);
